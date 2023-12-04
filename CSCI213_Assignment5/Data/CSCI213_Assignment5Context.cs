658c314006c141cd5a2d6a795d19075d2125703c
Logan Johnson <logan.k.johnson@ndsu.edu>
2023-12-03 21:21:14 -0600

    Created a "Admin" page names SongsController.

---
+++ CSCI213_Assignment5/Data/CSCI213_Assignment5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CSCI213_Assignment5.Models;
+
+namespace CSCI213_Assignment5.Data
+{
+    public class CSCI213_Assignment5Context : DbContext
+    {
+        public CSCI213_Assignment5Context (DbContextOptions<CSCI213_Assignment5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CSCI213_Assignment5.Models.Song> Song { get; set; } = default!;
+    }
+}
