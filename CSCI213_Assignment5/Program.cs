3780c8b1a4969e829a9f3f24d0c26f8e18347642
Logan Johnson <logan.k.johnson@ndsu.edu>
2023-12-03 20:44:17 -0600

    Personal Branch is being weird, Uploading this change to main. Created a Admin page of sorts that can modify songs

--- CSCI213_Assignment5/Program.cs
+++ CSCI213_Assignment5/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CSCI213_Assignment5.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CSCI213_Assignment5Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CSCI213_Assignment5Context") ?? throw new InvalidOperationException("Connection string 'CSCI213_Assignment5Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
