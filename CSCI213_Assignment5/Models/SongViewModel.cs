212d474e7ba480f34734caea1d2504a1411a4598
Logan Johnson <logan.k.johnson@ndsu.edu>
2023-12-05 15:25:52 -0600

    Almost there, Browse Page isnt quite working but is implemeted

---
+++ CSCI213_Assignment5/Models/SongViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CSCI213_Assignment5.Models
+{
+    public class SongViewModel
+    {
+        public List<Song>? Songs { get; set; }
+        public SelectList? Genres { get; set; }
+        public SelectList? Artists { get; set; }
+        public string? SongGenre { get; set; }
+        public string? SearchArtist { get; set; }
+    }
+}
